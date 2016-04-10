50aeb9761a3d9a2920b89fb37e43f3a71a396d3b
zhuwpeng <zhuwpeng@gmail.com>
2016-04-10 19:57:49 +0200

    Layout of page
    Created the layout of the page or skeleton with css and html.

---
+++ test.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Inzendopgave 051R7</title>
+
+</head>
+
+<body>
+    <div id="container">
+        <div class="header">
+
+            <div class="logo">
+                <h1>The header</h1>
+            </div>
+
+            <div class="navbar-wrap">
+                <ul class="nav">
+                    <li><a href=#>Home</a></li>
+                    <li><a href=#>About</a></li>
+                    <li><a href=#>Contact</a></li>
+                    <ul class="nav-login">
+                        <li><a href=#>Login</a></li>
+                        <li><a href=#>About</a></li>
+                    </ul>
+                </ul>
+
+            </div>
+        </div>
+
+        <div class="wrapper">
+            <div class="side-right">
+                <div class="panel-head">
+                    <h3>Bloggers</h3>
+                </div>
+                <div class="side-content">
+                    <ul>
+                        <li><a href='/ligretchen-lopez-li'>Gretchen Lopez</a></li>
+                        <li><a href='/lidorothy-mcbride-li'>Dorothy Mcbride</a></li>
+                        <li><a href='/lidianne-hamilton-li'>Dianne Hamilton</a></li>
+                        <li><a href='/lifrederick-wood-li'>Frederick Wood</a></li>
+                        <li><a href='/lialfred-byrd-li'>Alfred Byrd</a></li>
+                        <li><a href='/lityler-parks-li'>Tyer Parks</a></li>
+                        <li><a href='/litimmy-brooks-li'>Timmy Brooks</a></li>
+                        <li><a href='/lijeanne-pittman-li'>Jeanne Pittman</a></li>
+                        <li><a href='/lidexter-colon-li'>Dexter Colon</a></li>
+                        <li><a href='/licurtis-wright-li'>Curtis Wright</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="main-content">
+                <h2>The main content</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend metus et dui dictum rhoncus. In vel lacinia dui. Nulla hendrerit nibh eget turpis viverra, vitae eleifend sem dignissim. Ut id maximus nunc. Quisque placerat mi velit, vitae pellentesque tortor ultricies ac. Etiam sed mollis metus, vitae laoreet leo. Ut ultrices nulla est, ut bibendum nunc finibus vel. Mauris non aliquet urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis in urna nec leo convallis ullamcorper. Ut quis tincidunt elit. Fusce sem ex, vulputate sed mollis id, efficitur sed velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut bibendum ac nisl a porttitor. Mauris ut magna ligula.
+                </p>
+                <p>
+                    Suspendisse hendrerit venenatis volutpat. Aenean nec dui vitae quam venenatis imperdiet. Donec rutrum leo at varius tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut lorem metus, efficitur vel dui ut, mattis venenatis metus. In turpis felis, sodales vitae porta vitae, posuere in purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                </p>
+            </div>
+        </div>
+
+        <div class="footer">
+            <h2>This is the footer</h2>
+        </div>
+    </div>
+</body>
+
+</html>
